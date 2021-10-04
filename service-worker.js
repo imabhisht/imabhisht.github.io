b837fec21f6171df2dbb4554b27b5ba91b1ad11f
Abhisht <abhisht.chouhan@lynks.studio>
2021-10-04 09:25:19 +0530

    Fix Experience Year

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.32231d760057ed3a0cc813993dced38f.js"
+  "./precache-manifest.26cb9499ca817cd7b8f0cc943d2526d5.js"
 );
 
 self.addEventListener('message', (event) => {
